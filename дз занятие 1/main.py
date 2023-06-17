f9616fa4a5ed7d277a7b270332929d31c45ac864
loovi <123304693+zL4mpY@users.noreply.github.com>
2023-06-17 18:12:35 +0300

    Добавлена проверка на палиндром в функцию
    Добавлена проверка на палиндром в функцию, которая принимает на вход строку str_. Функция через цикл for перебирает каждый символ в перевёрнутой наоборот строке, которая была перевёрнута через функцию reversed(), позже добавляет этот символ в переменную reverse_str, которая является string, затем через условие сравнивает str_ и reverse_str, если строка - палиндром (т.е. эти 2 переменные равны, то функция возвращает True, иначе - False.

--- дз занятие 1/main.py
+++ дз занятие 1/main.py
@@ -1,9 +1,11 @@
+# добавил проверку на палиндром в функцию
+
 def palindrom(str_):
     str_ = str_.lower()
     reverse_str = ""
     for char in reversed(str_):
         reverse_str += char
-
+    
     if str_ == reverse_str:
         return True
 
