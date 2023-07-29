5075f5bba22d763d3e9ae036f779ada62650d387
zL4mpY <zL4mpY@github.com>
2023-07-29 17:48:21 +0300

    сделал дз занятия 8

---
+++ дз занятие 8/advertisements/app_advertisements/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
+
+class Advertisement(models.Model):
+    title = models.CharField(verbose_name='заголовок', max_length=128)
+    description = models.TextField('описание')
+    price = models.DecimalField('цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('торг', help_text='отметьте, если торг уместен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html('<span style="color:green; font-weight:bold"> Сегодня, в {}</span>', created_time)
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='Дата обновления')
+    def updated_date(self):
+        from django.utils import timezone
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime('%H:%M:%S')
+            return format_html('<span style="color:green; font-weight:bold"> Сегодня, в {}</span>', updated_time)
+        return self.updated_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    def __str__(self):
+        return f"Advertisement(id={self.id}, title={self.title}, price={self.price})"
+
+    class Meta:
+        db_table = "advertisements"
---
+++ дз занятие 8/advertisements/app_advertisements/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import index, top_sellers, advertisement_post, register, login, profile
+
+urlpatterns = [
+    path('', index, name='main-page'),
+    path('top-sellers/', top_sellers, name='top-sellers'),
+    path('advertisement-post/', advertisement_post, name='advertisement-post'),
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
+    path('profile/', profile, name='profile')
+]
